d235c1d1b4f4db4d77d37622214c70de73ca4899
Arnaud Loonstra <arnaud@sphaero.org>
2015-10-23 17:06:59 +0200

    renamed to sphaebian and fixed some late commands

--- seeds/late_command.sh
+++ seeds/late_command.sh
@@ -9,12 +9,11 @@
 set -ex
 
 modify_etc_skel() {
-  sed /etc/skel/.bashrc -i 's/#force_color_prompt=yes/force_color_prompt=yes/'
+  sed /etc/skel/.bashrc -i -e 's/#force_color_prompt=yes/force_color_prompt=yes/'
 }
 
 setup_automounting() {
-  onvm_chroot sh -l -e <<\EOF1
-cat <<EOF2 > /etc/polkit-1/localauthority/50-local.d/55-storage.pkla
+  cat <<EOF2 > /etc/polkit-1/localauthority/50-local.d/55-storage.pkla
 [Storage Permissions]
 Identity=unix-group:plugdev
 Action=org.freedesktop.udisks.filesystem-mount;org.freedesktop.udisks.drive-eject;org.freedesktop.udisks.drive-detach;org.freedesktop.udisks.luks-unlock;org.freedesktop.udisks.inhibit-polling;org.freedesktop.udisks.drive-set-spindown
@@ -66,9 +65,9 @@ fiddle_default_PATH() {
   # This sed match is clearly brittle and specific to the current debian 
   # /etc/profile
   sed /etc/profile -i -e \
-    's|PATH="/usr/.*games.*$|PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games"|'
+    's|PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"|PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/games:/usr/games"|'
   # need to fix ENV_PATH in /etc/login.defs
-  sed -i /etc/login.defs -e "s|^ENV_PATH.*|ENV_PATH        PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games|"
+  sed -i /etc/login.defs -e "s|ENV_PATH        PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games|ENV_PATH        PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games|"
 }
 
 # Not using, don't know if needed?
@@ -89,6 +88,16 @@ pcm.!default {
 EOF1
 }
 
+rename_sphaebian() {
+    sudo sed -i /etc/os-release -e 's|NAME="Debian GNU/Linux"|NAME="Sphaebian GNU/Linux"|'
+    . /etc/os-release
+    sed /etc/os-release -i -e "s|PRETTY_NAME=\"Debian .*|PRETTY_NAME=\"$NAME $VERSION\"|"
+    echo "$NAME $VERSION_ID8 \n \l" > /etc/issue
+    echo "" >> /etc/issue
+    echo "$NAME $VERSION_ID8" > /etc/issue.net
+    echo "" >> /etc/issue.net
+}
+
 modify_etc_skel
 #setup_automounting
 add_pi_user_to_groups
@@ -98,3 +107,4 @@ setup_sudoers
 tweak_inputrc
 fiddle_default_PATH
 #configure_sound
+rename_sphaebian
