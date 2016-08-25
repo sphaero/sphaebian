088589f86a84588f449157d53aed982839a9be28
A. Loonstra <arnaud@sphaero.org>
2016-08-25 14:45:31 +0200

    extenden x11 login service file, moved startx to profile

--- seeds/late_desktop_command.sh
+++ seeds/late_desktop_command.sh
@@ -11,22 +11,39 @@ set -ex
 systemd_autologin() {
   cat <<EOF> /etc/systemd/system/x11-login.service
 [Unit]
-After=systemd-user-sessions.service
-     
+Description=Autologin getty on tty8
+After=systemd-user-sessions.service plymouth-quit-wait.service
+After=rc-local.service
+
+Before=getty.target
+IgnoreOnIsolate=yes
+
+ConditionPathExists=/dev/tty8
+
 [Service]
 ExecStart=/sbin/agetty --autologin pi --noclear tty8 38400
-     
+Type=idle
+UtmpIdentifier=tty8
+TTYPath=/dev/tty8
+TTYReset=yes
+TTYVHangup=yes
+TTYVTDisallocate=yes
+KillMode=process
+IgnoreSIGPIPE=no
+SendSIGHUP=yes
+
 [Install]
 WantedBy=multi-user.target
+DefaultInstance=tty8
 EOF
   systemctl enable x11-login.service
-  if grep -q "/dev/tty6" /etc/bash.bashrc; then
+  if grep -q "/dev/tty8" /etc/bash.bashrc; then
      # return so we don't add to bashrc
      return
   fi
   # add to bashrc
-  cat <<"EOF" >> /etc/bash.bashrc
-if [[ ! ${DISPLAY} && `tty` == "/dev/tty6" ]]; then
+  cat <<"EOF" >> /etc/profile
+if [[ ! ${DISPLAY} && `tty` == "/dev/tty8" ]]; then
     exec startx
 fi
 EOF
